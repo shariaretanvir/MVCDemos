c0b83263d8c6a9d96dfc0975253743ef680003a9
Shariare tanvir akash <shariaretanvir_akash@yahoo.com>
2022-01-12 02:05:53 +0600

    Add project files.

---
+++ MVCDemos/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCDemos.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            byte[] data =  PrivacyModel.GetData();
+            ViewBag.file = Convert.ToBase64String(data);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Privacy(PrivacyModel model, IFormFile file)
+        {
+            string fileName = file.FileName;
+            byte[] datafile = null;
+            using (var target = new MemoryStream())
+            {
+                file.CopyTo(target);
+                datafile = target.ToArray();
+            }
+
+            PrivacyModel.Save(datafile);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCDemos/Models/PrivacyModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Data.SqlClient;
+
+namespace MVCDemos.Models
+{
+    public class PrivacyModel
+    {
+        public string  name { get; set; }
+        public string address { get; set; }
+
+
+
+        public static void Save(byte[] files)
+        {
+            using (SqlConnection con = new SqlConnection("Server=DESKTOP-C01O214\\AKASHSQLEXPRESS;Database=TestDB;Trusted_Connection=True;"))
+            {
+                //string query = "insert into tblFiles values (@Name, @ContentType, @Data)";
+                using (SqlCommand cmd = new SqlCommand("insfile"))
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Files", files);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+
+
+        public static byte[] GetData()
+        {
+            byte[] data = null;
+            using (SqlConnection con = new SqlConnection("Server=DESKTOP-C01O214\\AKASHSQLEXPRESS;Database=TestDB;Trusted_Connection=True;"))
+            {
+                string query = "select Files from TableWithFile where ID=1";
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    //cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    //cmd.Parameters.AddWithValue("@Files", files);
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        sdr.Read();
+                        data = (byte[])sdr["Files"];
+                        //contentType = sdr["ContentType"].ToString();
+                        //fileName = sdr["Name"].ToString();
+                    }
+                    con.Close();
+                }
+            }
+            return data;
+        }
+    }
+}
